393aedf78fbe614743d33de2775cdad71048b3ac
Steve <sshoemaker@smu.edu>
2020-05-20 23:46:41 -0700

    Converted to words pointing to Documents

---
+++ DocumentIndex.cpp
@@ -0,0 +1,5 @@
+//
+// Created by stephenshoemaker on 5/20/20.
+//
+
+#include "DocumentIndex.h"
---
+++ DocumentIndex.h
@@ -0,0 +1,14 @@
+//
+// Created by stephenshoemaker on 5/20/20.
+//
+
+#ifndef SEARCHENGINE_DOCUMENTINDEX_H
+#define SEARCHENGINE_DOCUMENTINDEX_H
+
+
+class DocumentIndex {
+
+};
+
+
+#endif //SEARCHENGINE_DOCUMENTINDEX_H
